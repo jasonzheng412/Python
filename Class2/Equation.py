b9c0dae998466208cb4a4d903664095d5c7a917e
jasonzheng412 <jasonzheng221@outlook.com>
2020-06-29 23:14:02 -0700

    Equation.py

--- Class2/Equation.py
+++ Class2/Equation.py
@@ -1,9 +1,18 @@
 print('This is a tool to solve quadratic equations.')
 
+import math
 a = float(input('Please input a:'))
 b = float(input('Please input b:'))
 c = float(input('Please input c:'))
-
 print('Your equation is %gx^2 + %gx + %g = 0.' % (a, b, c))
-
-print('The solutions to the equations are:')
+x = b*b - 4*a*c
+if  x > 0: 
+    y = math.sqrt(x)
+    z = (-b+y)/(2*a)
+    w = (-b-y)/(2*a) 
+    print('The solutions to the equation are: %g, %g' % (z, w)) 
+elif x == 0:
+    z = -b/(2*a)
+    print('The solution to the equation is: %g' % z)
+else:
+    print('There is no solution to the equation.')
