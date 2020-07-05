fec3205facffce32288a1c4da60ae67a1410ac94
jasonzheng412 <jasonzheng221@outlook.com>
2020-07-05 15:24:10 -0700

    HWMain.py

---
+++ Class5/HWMain.py
@@ -0,0 +1,9 @@
+import Calculator
+
+print('This is a tool to solve quadratic equations.')
+
+a = float(input('Please input a:'))
+b = float(input('Please input b:'))
+c = float(input('Please input c:'))
+
+Calculator.calculator(a, b, c)
