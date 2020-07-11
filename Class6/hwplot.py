290890207b5e36433c6fd9ba2bc6b527986093c2
jasonzheng412 <jasonzheng221@outlook.com>
2020-07-11 14:52:12 -0700

    hwplot.py

---
+++ Class6/hwplot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+list_x = []
+for i in range (200):
+    list_x.append(i*0.5 - 5)
+
+list_y = []
+for x in list_x:
+    list_y.append(x * x)
+
+plt.plot(list_x, list_y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('my function plot')
+
+#plt.show()
+plt.savefig('hwplot.png')
