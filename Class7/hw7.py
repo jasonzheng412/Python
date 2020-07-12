3d29c1fc475bf0cae3bc2d5a1d7a0d62e6f2ea99
jasonzheng412 <jasonzheng221@outlook.com>
2020-07-12 15:26:49 -0700

    hw7.py

--- Class7/hw7.py
+++ Class7/hw7.py
@@ -9,3 +9,15 @@ student_dict[1] = jason
 amy = school.Student(student_id=2, name='Amy', age=15, score=20)
 
 student_dict[2] = amy
+
+yin = school.Student(student_id=3, name='Yin', age=45, score=30)
+
+student_dict[3] = yin
+
+direct = int(input('What is your student ID?'))
+
+if direct not in student_dict:
+        print('This student ID cannot be found.')
+
+else:
+        student_dict.get(direct).print()
