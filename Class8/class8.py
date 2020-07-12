078784203f02bc161594a07566ac7755764f37cd
jasonzheng412 <jasonzheng221@outlook.com>
2020-07-12 16:13:44 -0700

    class8.py

--- Class8/class8.py
+++ Class8/class8.py
@@ -1,1 +1,18 @@
+student_set = set()
 
+student_set.add('Jason')
+
+# print(student_set):
+
+student_set.add('Jason')
+
+student_set.add('Amy')
+
+print(student_set)
+
+student_set.remove('Jason')
+
+if 'Jason' in student_set:
+    print('I found Jason!')
+else:
+    print('I can\'t find Jason...')
