19f3a7e033e6ac17469131f81d90a80380fd8fdf
jasonzheng412 <jasonzheng221@outlook.com>
2020-08-01 15:47:34 -0700

    Class9

--- Class9/example.py
+++ Class9/example.py
@@ -1,1 +1,40 @@
+from PIL import Image
+from PIL import ImageFilter
+im = Image.open('Twomad.jpg')
 
+im.show()
+
+print(im.format, im.size, im.mode)
+
+# thumbnail
+
+size = (128, 128)
+im.thumbnail(size)
+
+im.show()
+
+im.save('thumbnail.jpg', 'JPEG')
+
+# rotating
+rotated = im.rotate(45)
+rotated.save('rotated.jpg', 'JPEG')
+
+rotated.show()
+
+# transpose 
+transposed = im.transpose(Image.FLIP_TOP_BOTTOM)
+transposed.save('upsidedown.jpg', 'JPEG')
+
+transposed.show()
+
+# filter
+detail = im.filter(ImageFilter.DETAIL)
+detail.save('detail.jpg', 'JPEG')
+
+detail.show()
+
+# black and white
+black_white = im.convert('L')
+black_white.save('black_white.jpg', 'JPEG')
+
+black_white.show()
